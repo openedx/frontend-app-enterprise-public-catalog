ea21389d847b4ef867728c21f342badcc984296f
binodpant <bpant@edx.org>
2021-03-11 09:22:24 -0500

    test: PageWrapper

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -4,11 +4,13 @@ import { Helmet } from 'react-helmet';
 
 import { PAGE_TITLE } from '../constants';
 
+export const DATA_TEST_ID = 'enterprise-catalogs-content';
+
 // eslint-disable-next-line react/prop-types
 const Wrapper = ({ children }) => (
   <Container size="lg" className="enterprise-catalogs mt-3">
     <Helmet title={PAGE_TITLE} />
-    <div className="py-5">
+    <div data-testid={DATA_TEST_ID} className="py-5">
       {children}
     </div>
   </Container>
