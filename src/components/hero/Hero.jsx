bc1dc44c1f1bec1d3e130a988cbbccf242f1e836
Lael Birch <lbirch@edx.org>
2021-04-16 09:32:55 -0400

    fix: update images to use srcset (#36)
    * Update images to use srcset
    
    * Set the minimal header variable
    
    AUTHN_MINIMAL_HEADER is needed on dev, but MINIMAL_HEADER is needed on prod. Included here mostly to remind people that it's needed on prod.
    
    * Use constant for the image class

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,21 +4,38 @@ import {
 import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import SmallHeroImage from '../../assets/hero-image-cluster-1x.png';
-import LargeHeroImage from '../../assets/hero-image-cluster-2x.png';
+import SmallHeroImageLoRes from '../../assets/hero-image-small-lo-res.png';
+import LargeHeroImageLoRes from '../../assets/hero-image-large-lo-res.png';
+import SmallHeroImageHiRes from '../../assets/hero-image-hi-res.png';
+import LargeHeroImageHiRes from '../../assets/hero-image-144px-hi-res.png';
 import { Highlighted } from '../helperComponents';
 import messages from './Hero.messages';
+
+const IMAGE_WRAPPER_CLASS = 'hero__image-wrapper';
+const IMAGE_CLASS = 'hero__image';
 
 const Hero = ({ intl, text, highlight }) => (
   <section className="hero">
     <Container size="lg" className="hero__content">
       <h1 className="display-3"><Highlighted text={text} highlight={highlight} /></h1>
       <div>
-        <Large>
-          <Image className="hero__image" src={SmallHeroImage} alt={intl.formatMessage(messages['hero.image.alt'])} />
+        <Large className={IMAGE_WRAPPER_CLASS}>
+          <Image
+            className={IMAGE_CLASS}
+            srcSet={`${SmallHeroImageLoRes} 1000w, ${SmallHeroImageHiRes} 2000w`}
+            src={SmallHeroImageLoRes}
+            alt={intl.formatMessage(messages['hero.image.alt'])}
+            size="33vw"
+          />
         </Large>
-        <ExtraLarge>
-          <Image className="hero__image" src={LargeHeroImage} alt={intl.formatMessage(messages['hero.image.alt'])} />
+        <ExtraLarge className={IMAGE_WRAPPER_CLASS}>
+          <Image
+            className={IMAGE_CLASS}
+            srcSet={`${LargeHeroImageLoRes} 1000w, ${LargeHeroImageHiRes} 2000w`}
+            src={LargeHeroImageLoRes}
+            alt={intl.formatMessage(messages['hero.image.alt'])}
+            size="33vw"
+          />
         </ExtraLarge>
       </div>
     </Container>
