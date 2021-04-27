e66ff882dd3b08b85961b319ca81fc6333cc1ab7
binodpant <bpant@edx.org>
2021-04-27 15:12:01 -0400

    feat: :sparkles: Add segment tracking by passing trackingEvent context param
    SearchData context now supports tracking events via custom naming using `trackingName`
    
    ENT-4126

--- src/constants.js
+++ src/constants.js
@@ -8,6 +8,9 @@ export const NUM_RESULTS_PER_PAGE = 40;
  * Spec: https://openedx.atlassian.net/wiki/spaces/AN/pages/2624455676/Enterprise+public+catalog+Event+Design
  */
 export const PRODUCT_EVENTS = {
-  ctaButtonPressed: 'edx.enterprise.public_catalog.cta_button_pressed',
+  ctaButtonPressed: 'edx.enterprise.explore-catalog.cta_button_pressed',
 };
+export const TRACKING_APP_NAME = 'explore-catalog';
+// end: tracking related
+
 export const QUERY_UUID_REFINEMENT = 'enterprise_catalog_query_uuids';
