e198b2b530480f368f620bace3902eb3d7cf0c7b
Talia <trhodes@edx.org>
2021-06-04 15:30:46 -0400

    fix: query with filters to get accurate pill counts

--- src/components/catalogs/data/hooks.jsx
+++ src/components/catalogs/data/hooks.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable import/prefer-default-export */
-import { useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 
 import algoliasearch from 'algoliasearch/lite';
 
 import { getConfig } from '@edx/frontend-platform';
+import { SearchContext } from '@edx/frontend-enterprise';
+import { QUERY_UUID_REFINEMENT } from '../../../constants';
 
 const useAlgoliaIndex = () => {
   // note: calling the getConfig outside of a hook/render function won't work
@@ -21,7 +23,34 @@ const useMarketingSite = () => {
   return config.HUBSPOT_MARKETING_URL;
 };
 
+const useDefaultSearchFilters = () => {
+  const { refinementsFromQueryParams } = useContext(SearchContext);
+  const config = getConfig();
+  const allQueryUuids = [
+    config.EDX_FOR_BUSINESS_UUID,
+    config.EDX_FOR_ONLINE_EDU_UUID,
+    config.EDX_ONLINE_ESSENTIALS_UUID,
+  ];
+  const filters = (uuids) => {
+    const queryReducer = (result, uuid, index) => {
+      const isLastCatalogQuery = index === uuids.length - 1;
+      let query = ''.concat(result, 'enterprise_catalog_query_uuids:').concat(uuid);
+      if (!isLastCatalogQuery) {
+        query += ' OR ';
+      }
+      return query;
+    };
+
+    return uuids.reduce(queryReducer, '');
+  };
+  if (!refinementsFromQueryParams[QUERY_UUID_REFINEMENT]) {
+    return filters(allQueryUuids);
+  }
+  return filters(refinementsFromQueryParams[QUERY_UUID_REFINEMENT]);
+};
+
 export {
   useAlgoliaIndex,
   useMarketingSite,
+  useDefaultSearchFilters,
 };
