b09e2ff738e9ea0e343066f2f84cbd96782c61fb
Binod Pant <bpant@edx.org>
2021-12-10 09:06:05 -0500

    feat: Add hotjar initialization (#161)
    * feat: Add hotjar initializaiton
    
    ENT-3829
    
    * feat: use empty hotjar id

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch } from 'react-router-dom';
 import { AppProvider, PageRoute } from '@edx/frontend-platform/react';
 
 import Header from '@edx/frontend-component-header';
 import Footer from '@edx/frontend-component-footer';
 
+import useHotjar from 'react-use-hotjar';
 import CatalogPage from '../catalogPage/CatalogPage';
 import NotFoundPage from '../NotFoundPage';
 
@@ -20,6 +21,12 @@ export const EnterpriseCatalogsApp = () => (
 );
 
 export default function App() {
+  if (process.env.HOTJAR_APP_ID) {
+    const { initHotjar } = useHotjar();
+    useEffect(() => {
+      initHotjar(process.env.HOTJAR_APP_ID, process.env.HOTJAR_VERSION, process.env.HOTJAR_DEBUG);
+    }, [initHotjar]);
+  }
   return (
     <AppProvider>
       <EnterpriseCatalogsApp />
