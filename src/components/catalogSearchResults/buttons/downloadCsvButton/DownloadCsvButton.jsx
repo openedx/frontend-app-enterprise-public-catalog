58a8e554ca8c3543df0bf3c3ae209d0d930cbda5
John Nagro <jnagro@edx.org>
2022-01-21 14:52:06 -0500

    fix: stream csv downloads
    - quality
    
    ENT-5222

--- src/components/catalogSearchResults/buttons/downloadCsvButton/DownloadCsvButton.jsx
+++ src/components/catalogSearchResults/buttons/downloadCsvButton/DownloadCsvButton.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import { logError } from '@edx/frontend-platform/logging';
 import {
-  Toast, Button, Icon, Spinner, useToggle,
+  Toast, Button, useToggle,
 } from '@edx/paragon';
-import { Download, Check } from '@edx/paragon/icons';
+import { Download } from '@edx/paragon/icons';
 
 import EnterpriseCatalogApiService from '../../../../data/services/EnterpriseCatalogAPIService';
 
@@ -36,7 +35,7 @@ const DownloadCsvButton = ({ facets, query }) => {
     formatFilterText(facets);
     open();
     // https://enterprise-catalog.edx.org/api/v1/enterprise-catalogs/catalog_csv/?availability=Available%20Now&availability=Upcoming&enterprise_catalog_query_titles=A%20la%20carte
-    let downloadUrl = EnterpriseCatalogApiService.generateCsvDownloadLink(facets, query);
+    const downloadUrl = EnterpriseCatalogApiService.generateCsvDownloadLink(facets, query);
     global.location.href = downloadUrl;
   };
   const toastText = `Downloaded with filters: ${filters}. Check website for the most up-to-date information on courses.`;
@@ -48,10 +47,7 @@ const DownloadCsvButton = ({ facets, query }) => {
          {toastText}
        </Toast>
        )}
-      <Button
-        className="ml-2 download-button"
-        iconBefore={Download}
-        onClick={handleClick}>
+      <Button className="ml-2 download-button" iconBefore={Download} onClick={handleClick}>
         Download results
       </Button>
     </>
