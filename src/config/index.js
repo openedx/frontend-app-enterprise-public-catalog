4fd03de2f337cbb5f4940050162dcc9abe7d12f4
knguyen2 <knguyen2@2u.com>
2023-06-08 20:45:08 +0000

    fix: replace query-string with URLSearchParams

--- src/config/index.js
+++ src/config/index.js
@@ -1,24 +1,19 @@
-import qs from 'query-string';
+import { hasFeatureFlagEnabled } from '@edx/frontend-enterprise-utils';
 import {
   FEATURE_ENABLE_PROGRAMS,
   FEATURE_EXEC_ED_INCLUSION,
   FEATURE_PROGRAM_TYPE_FACET,
 } from './constants';
 
-const hasFeatureFlagEnabled = (featureFlag) => {
-  const { features } = qs.parse(window.location.search);
-  return features && features.split(',').includes(featureFlag);
-};
-
 const features = {
   ENABLE_PROGRAMS:
-    process.env.FEATURE_ENABLE_PROGRAMS === 'true'
+    process.env.FEATURE_ENABLE_PROGRAMS
     || hasFeatureFlagEnabled(FEATURE_ENABLE_PROGRAMS),
   PROGRAM_TYPE_FACET:
-    process.env.FEATURE_PROGRAM_TYPE_FACET === 'true'
+    process.env.FEATURE_PROGRAM_TYPE_FACET
     || hasFeatureFlagEnabled(FEATURE_PROGRAM_TYPE_FACET),
   EXEC_ED_INCLUSION:
-    process.env.EXEC_ED_INCLUSION === 'true'
+    process.env.EXEC_ED_INCLUSION
     || hasFeatureFlagEnabled(FEATURE_EXEC_ED_INCLUSION),
 };
 
--- src/utils/catalogUtils.test.js
+++ src/utils/catalogUtils.test.js
@@ -1,9 +1,24 @@
 import { EXEC_ED_TITLE } from '../constants';
-import { convertLearningTypesToFilters } from './catalogUtils';
+import { convertLearningTypesToFilters, createQueryParams } from './catalogUtils';
 
 describe('catalogUtils', () => {
   it('converts lists of learning types to algolia filters', () => {
     const algoliaFilter = convertLearningTypesToFilters(['a', 'b', EXEC_ED_TITLE]);
     expect(algoliaFilter).toEqual('a OR b OR "Executive Education"');
+  });
+
+  it('parses an object with an array of values and returns query string', () => {
+    const options = {
+      enterprise_catalog_query_titles: [
+        'A la carte',
+      ],
+      availability: [
+        'Available Now',
+        'Starting Soon',
+      ],
+    };
+    const expectedQueryParams = 'enterprise_catalog_query_titles=A+la+carte&availability=Available+Now&availability=Starting+Soon';
+    const queryParams = createQueryParams(options);
+    expect(queryParams).toEqual(expectedQueryParams);
   });
 });
