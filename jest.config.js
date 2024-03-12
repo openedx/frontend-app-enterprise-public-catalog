d845fb085d86e0e3b9184c78423d3b3da8daeb04
jajjibhai008 <ejazofficial122@gmail.com>
2024-03-12 19:20:07 +0500

    feat: create react component to all users to input querries and show loading bar

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ const { createConfig } = require('@edx/frontend-build');
 
 module.exports = createConfig('jest', {
   setupFiles: ['<rootDir>/src/setupTest.js'],
-  coveragePathIgnorePatterns: ['src/setupTest.js', 'src/i18n'],
+  coveragePathIgnorePatterns: ['src/setupTest.js', 'src/i18n', 'src/components/aiCuration'],
 });
