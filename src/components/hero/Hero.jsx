fae217393a296d5eb1111a8010b2ed843ee7d4e3
mchilagani-sonata <mchilagani-sonata@2u.com>
2025-11-28 13:56:12 +0000

    refactor(tests): remove unused code

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,29 +1,15 @@
 import { useIntl } from '@edx/frontend-platform/i18n';
 import {
-  Container, Image, useMediaQuery, breakpoints,
+  breakpoints,
+  Container, Image, useMediaQuery,
 } from '@openedx/paragon';
 import PropTypes from 'prop-types';
 import LargeHeroImageHiRes from '../../assets/hero-image-144px-hi-res.jpg';
-import SmallHeroImageHiRes from '../../assets/hero-image-hi-res.jpg';
 import LargeHeroImageLoRes from '../../assets/hero-image-large-lo-res.jpg';
-import SmallHeroImageLoRes from '../../assets/hero-image-small-lo-res.jpg';
 import { Highlighted } from '../helperComponents';
 import messages from './Hero.messages';
 
 const IMAGE_CLASS = 'hero__image';
-
-const SmallImage = ({ alt }) => (
-  <Image
-    className={IMAGE_CLASS}
-    srcSet={`${SmallHeroImageLoRes} 1000w, ${SmallHeroImageHiRes} 2000w`}
-    src={SmallHeroImageLoRes}
-    alt={alt}
-    sizes="23vw"
-  />
-);
-SmallImage.propTypes = {
-  alt: PropTypes.string.isRequired,
-};
 
 const LargeImage = ({ alt }) => (
   <Image
@@ -42,20 +28,6 @@ const Desktop = ({ children }) => {
   return isDesktop ? children : null;
 };
 Desktop.propTypes = {
-  children: PropTypes.shape({
-    props: PropTypes.shape({
-      alt: PropTypes.string,
-    }),
-  }).isRequired,
-};
-const Tablet = ({ children }) => {
-  const isTablet = useMediaQuery({
-    minWidth: breakpoints.medium.minWidth,
-    maxWidth: breakpoints.medium.maxWidth,
-  });
-  return isTablet ? children : null;
-};
-Tablet.propTypes = {
   children: PropTypes.shape({
     props: PropTypes.shape({
       alt: PropTypes.string,
